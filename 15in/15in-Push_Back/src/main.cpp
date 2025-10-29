53d5d18df73f2ddfec28d14c92fb89aff47b7ff1
Daniel Kohler <danielkohler737@gmail.com>
2025-10-29 00:32:41 -0500

    15 in wheelbase set up in LemLib

--- 15in/15in-Push_Back/src/main.cpp
+++ 15in/15in-Push_Back/src/main.cpp
@@ -1,4 +1,68 @@
 #include "main.h"
+#include "lemlib/api.hpp"
+
+pros::MotorGroup left_motor_group({-1, 2, -3, 4, -5}, pros::MotorGearset::blue); // left motors use 600 RPM cartridges
+pros::MotorGroup right_motor_group({-6, 7 -8, 9, -10}, pros::MotorGearset::blue); // right motors use 600 RPM cartridges
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
+                              &right_motor_group, // right motor group
+                              11.95, // 10 inch track width
+                              lemlib::Omniwheel::NEW_275, // using new 4" omnis
+                              600, // drivetrain rpm is 360
+                              2 // horizontal drift is 2
+);
+
+// create an imu on port 10
+pros::Imu imu(11);
+
+// horizontal tracking wheel encoder
+pros::Rotation horizontal_encoder(12);
+// horizontal tracking wheel encoder
+pros::Rotation vertical_encoder(13);
+
+// horizontal tracking wheel
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_2, -1.906);
+// vertical tracking wheel
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_2, -0.15);
+
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            &horizontal_tracking_wheel, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -7,7 +71,19 @@
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
 }
 
 /**
